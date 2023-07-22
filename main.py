412a8ea6ed8e03017372b2d444c6931ee9ee19cc
Kevin Acevedo <acevedo@mit.edu>
2023-07-21 23:38:23 -0400

    created main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+##########################################
+###  Analysis on Spotify account data  ### 
+##########################################
+
+import json
+
+# make functions here
+
+
+#######################################
+### Save the analysis to a txt file ###
+#######################################
+if __name__ == "__main__":
+    pass
