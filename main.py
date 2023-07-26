58d15814dbb2c00037d8bea495079f7c3e4557fc
Kevin Acevedo <acevedo@mit.edu>
2023-07-26 18:01:06 -0400

    created get_all_data

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 ###  Analysis on Spotify account data  ### 
 ##########################################
 
+import os
 import json
+import pandas as pd
 
-# make functions here
+def get_all_data():
+    df = pd.DataFrame()
 
+    for file in os.listdir("MyData"):
+        if file.startswith("Streaming_History_Audio") and file.endswith(".json"):
+            df = pd.concat([df, pd.read_json(f"MyData/{file}")])
+
+    return df
 
 #######################################
 ### Save the analysis to a txt file ###
 #######################################
 if __name__ == "__main__":
-    pass
+    print(get_all_data())
